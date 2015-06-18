591b336ef0049eae34b09057de8d1cc8f5f3d557
Daniel Salvagni <danielsalvagni@gmail.com>
2015-06-18 19:55:19 -0300

    Correção no estilo Alteração da propriedade que controla o status da todo

--- app/src/modules/todo/controller/TodoCtrl.js
+++ app/src/modules/todo/controller/TodoCtrl.js
@@ -11,19 +11,19 @@ define([], function () {
 
         $scope.add = function (item, $event) {
             if ($event.keyCode !== 13) return false;
-            $scope.items.push({description: item.description, done: false, selected: false});
+            $scope.items.push({description: item.description, done: false});
             $scope.item = {};
         };
 
         $scope.getSelectedTasks = function () {
             return $scope.items.filter(function (item) {
-                return item.selected;
+                return item.done;
             });
         };
         $scope.deleteSelected = function (items) {
             if (!confirm('Are you sure?')) return false;
             $scope.items = items.filter(function (item) {
-                return !item.selected;
+                return !item.done;
             });
         };
         getAllTasks();
