072bfe631b0012130fcae5346c917deca941d364
Daniel Salvagni <danielsalvagni@gmail.com>
2015-09-12 17:23:00 -0300

    Added bowerRequirejs task to grunt. Changed grunt default task to execute bowerRequirejs. Updated readme file to explain the changes with bower support.

--- app/main.js
+++ app/main.js
@@ -1,24 +1,26 @@
 requirejs.config({
     baseUrl: './',
     paths: {
-        angular: 'vendor/angular/angular.min',
-        jquery: 'vendor/jquery/jquery.min',
-        bootstrap: 'vendor/bootstrap/bootstrap.min',
-        'angular-route': 'vendor/angular/angular-route.min',
-        'angular-ui-router': 'vendor/angular/angular-ui-router.min'
+        angular: 'vendor/angular/angular',
+        'angular-mocks': 'vendor/angular-mocks/angular-mocks',
+        'angular-route': 'vendor/angular-route/angular-route',
+        'angular-ui-router': 'vendor/angular-ui-router/release/angular-ui-router',
+        bootstrap: 'vendor/bootstrap/dist/js/bootstrap'
     },
+    packages: [
+
+    ],
     shim: {
-        angular: {
-            exports: 'angular'
-        },
         'angular-ui-router': {
-            deps: ['angular', 'angular-route']
+            deps: [
+                'angular',
+                'angular-route'
+            ]
         },
         'angular-route': {
-            deps: ['angular']
-        },
-        'bootstrap': {
-            deps: ['jquery']
+            deps: [
+                'angular'
+            ]
         }
     }
 });
