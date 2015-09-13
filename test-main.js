9722edbd7c5e65714d88af0e1a94f28359371df0
Daniel Salvagni <danielsalvagni@gmail.com>
2015-09-13 10:34:45 -0300

    Fixed test-main file for the current project dependencies

--- test-main.js
+++ test-main.js
@@ -11,23 +11,26 @@ for (var file in window.__karma__.files) {
 requirejs.config({
     baseUrl: '/base/app/',
     paths: {
-        angular: 'vendor/angular/angular.min',
-        'angular-route': 'vendor/angular/angular-route.min',
-        'angular-ui-router': 'vendor/angular/angular-ui-router.min',
-        'mainApp': 'app.js'
+        angular: 'vendor/angular/angular',
+        'angular-mocks': 'vendor/angular-mocks/angular-mocks',
+        'angular-route': 'vendor/angular-route/angular-route',
+        'angular-ui-router': 'vendor/angular-ui-router/release/angular-ui-router',
+        bootstrap: 'vendor/bootstrap/dist/js/bootstrap'
     },
+    packages: [
+
+    ],
     shim: {
-        angular: {
-            exports: 'angular'
-        },
         'angular-ui-router': {
-            deps: ['angular', 'angular-route']
+            deps: [
+                'angular',
+                'angular-route'
+            ]
         },
         'angular-route': {
-            deps: ['angular']
-        },
-        'bootstrap': {
-            deps: ['jquery']
+            deps: [
+                'angular'
+            ]
         }
     },
     deps: tests,
