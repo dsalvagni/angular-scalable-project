ed73e62295fdf7ac65679e90d91c4c7b8496e86a
Daniel Salvagni <danielsalvagni@gmail.com>
2015-09-13 11:06:32 -0300

    Fixed karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,76 @@
 // Karma configuration
-// Generated on Sat Sep 12 2015 20:02:02 GMT-0300 (Hora oficial do Brasil)
+// Generated on Tue May 26 2015 21:19:23 GMT-0300 (Hora oficial do Brasil)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
-
-
-    // frameworks to use
-    frameworks: ['jasmine', 'requirejs'],
+        // base path, that will be used to resolve files and exclude
+        basePath: './',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      {pattern: 'app/*.js', included: false},
-      {pattern: 'app/src/**/*.js', included: false},
-      {pattern: 'app/vendor/**/*.js', included: false}
-    ],
+        // frameworks to use
+        frameworks: ['jasmine', 'requirejs'],
 
 
-    // list of files to exclude
-    exclude: [
-      'app/vendor/requirejs/*.js'
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'app/vendor/angular/angular.min.js',
+            'app/vendor/angular-mocks/angular-mocks.js',
+            'app/vendor/angular-route/angular-route.min.js',
+            'app/vendor/angular-ui-router/release/angular-ui-router.min.js',
+            {pattern: 'app/app.js', included: false},
+            {pattern: 'app/vendor/**/*.js', included: false},
+            {pattern: 'app/src/**/*.js', included: false},
+            'test-main.js'
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [
+            'app/main.js',
+            'app/vendor/requirejs/*.js'
+        ],
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress', 'junit'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['PhantomJS'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        browsers: ['PhantomJS'],
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
